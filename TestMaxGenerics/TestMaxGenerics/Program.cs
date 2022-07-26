50fe6348c98260a8bc09120bd54e1af7abb93ff3
ravi04ez <ezekie.em@gmail.com>
2022-07-26 11:45:51 +0530

    Wlcm Msg of Generics

---
+++ TestMaxGenerics/TestMaxGenerics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestMaxGenrics
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t###################################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO PROGRAM FIND MAXIMUM USING GENERICS");
+            Console.WriteLine("\t\t\t\t###################################################################");
+            Console.ReadKey();
+        }
+    }
+}
