1d979d5fb37e042b042f86c424c1c5bd79c5b194
ravi04ez <ezekie.em@gmail.com>
2022-07-27 16:12:22 +0530

    Added Using More Than Three Parameters

---
+++ TestMaxGenerics/TestMaxGenerics/GenericFindMax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaxGenerics
+{
+
+    internal class GenericFindMax<T> where T : IComparable
+    {
+        public T[] Values;
+
+        public GenericFindMax(T[] Values)
+        {
+            this.Values = Values;
+        }
+
+        public T[] SortValues(T[] Values)
+        {
+            Array.Sort(Values);
+            return Values;
+        }
+
+        // Creating Method to Sort & Find Max Value..
+        public T FindMax(T[] values)
+        {
+            var sortedValues = SortValues(this.Values);
+            return sortedValues[sortedValues.Length - 1];
+        }
+
+        public T FindMaxMethod()
+        {
+            var maxValue = FindMax(this.Values);
+            return maxValue;
+        }
+    }
+}
--- TestMaxGenerics/TestMaxGenerics/Program.cs
+++ TestMaxGenerics/TestMaxGenerics/Program.cs
@@ -21,6 +21,7 @@ namespace TestMaxGenrics
                 Console.WriteLine("3.Find Maximum Among Three String Value");
                 Console.WriteLine("4.Find Maximum Among Three Value Using Generics Method");
                 Console.WriteLine("5.Find Maximum among Three Value using Generic class");
+                Console.WriteLine("6. Find Maximum from More Than Three Parameters Using Generic");
                 Console.Write("\nEnter Your Choice : ");
                 Choice = Convert.ToInt32(Console.ReadLine()); // store iput From User & Convert Into Integer type
 
@@ -38,6 +39,7 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(40, 60, 50));
 
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxInt.FindMaxAmongInt(80, 70, 90));
+                        Console.WriteLine("######################################################\n");
 
                         break;
 
@@ -51,6 +53,7 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxFloat.FindMaxAmongFloat(4.1f, 6.1f, 5.1f));
 
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxFloat.FindMaxAmongFloat(8.1f, 7.1f, 9.1f));
+                        Console.WriteLine("######################################################\n");
                         break;
 
                     case 3:
@@ -63,6 +66,7 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxString.FindMaxAmongString("Amber", "Charlie", "Bruno"));
 
                         Console.WriteLine("Maximum Integer Number = {0}", objFindMaxString.FindMaxAmongString("Dwayne", "Cherry", "Scarlet"));
+                        Console.WriteLine("######################################################\n");
                         break;
 
                     case 4:
@@ -73,7 +77,7 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum Integer Number = {0}", objfindmax.FindMaxGeneric(40, 60, 50));
 
                         Console.WriteLine("Maximum Integer Number = {0}", objfindmax.FindMaxGeneric(70, 80, 90));
-                        Console.WriteLine("###################################################");
+                        Console.WriteLine("###################################################\n");
 
 
                         // for Float Value
@@ -82,7 +86,7 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum Float Number = {0}", objfindmax.FindMaxGeneric(4.1f, 6.1f, 5.1f));
 
                         Console.WriteLine("Maximum Float Number = {0}", objfindmax.FindMaxGeneric(7.1f, 8.1f, 9.1f));
-                        Console.WriteLine("###################################################");
+                        Console.WriteLine("###################################################\n");
 
 
 
@@ -92,27 +96,48 @@ namespace TestMaxGenrics
                         Console.WriteLine("Maximum String Value = {0}", objfindmax.FindMaxGeneric("Amber", "Charlie", "Bruno"));
 
                         Console.WriteLine("Maximum String Value = {0}", objfindmax.FindMaxGeneric("Dwayne", "Cherry", "Scarlet"));
-
+                        Console.WriteLine("######################################################\n");
                         break;
                     case 5:
                         //Test case for Integer
                         GenricClassMax<int> maxInt = new GenricClassMax<int>(10, 20, 30);
                         Console.WriteLine("Maximum integer Number =  " + maxInt.TestMaximum());
-                        Console.WriteLine("\n###################################################");
+                        Console.WriteLine("###################################################\n");
 
                         //Test case for Float
                         GenricClassMax<float> maxFloat = new GenricClassMax<float>(1.1f, 2.1f, 3.1f);
                         Console.WriteLine("Maximum Float Number =  " + maxFloat.TestMaximum());
-                        Console.WriteLine("\n###################################################");
+                        Console.WriteLine("###################################################\n");
 
                         //Test case for String
                         GenricClassMax<string> maxString = new GenricClassMax<string>("Apple", "Banana", "Peach");
                         Console.WriteLine("Maximum String Value = " + maxString.TestMaximum());
+                        Console.WriteLine("######################################################\n");
+                        break;
+
+                    case 6:
+                        //Test case for Integer
+                        int[] intArray = { 100, 120, 150, 130, 240 };
+                        GenericFindMax<int> objMaxInt = new GenericFindMax<int>(intArray);
+                        Console.WriteLine("Maximum Integer Value = " + objMaxInt.FindMaxMethod());
+                        Console.WriteLine("#####################################################\n");
+
+                        //Test case for Float
+                        float[] floatArray = { 11.1f, 52.2f, 45.5f, 23.3f, 14.4f };
+                        GenericFindMax<float> objMaxFloat = new GenericFindMax<float>(floatArray);
+                        Console.WriteLine("Maximum Float value = " + objMaxFloat.FindMaxMethod());
+                        Console.WriteLine("######################################################\n");
+
+                        //Test case for String
+                        string[] stringArray = { "Daniel", "David", "Jhon", "Dwyane", "Charlie" };
+                        GenericFindMax<string> objMaxString = new GenericFindMax<string>(stringArray);
+                        Console.WriteLine("Maximum String Value = " + objMaxString.FindMaxMethod());
+                        Console.WriteLine("######################################################\n");
                         break;
 
 
                     default:
-                        Console.WriteLine("Please Enter Correct Question Number");
+                        Console.WriteLine("\nPlease Enter Correct Question Number");
                         break;
                 }
             } while (Choice != 0);
